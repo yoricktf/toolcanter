e3a6f9d3f12e85de8772583d0864eb3d693d6278
yoricktf <yorick.tenfeld@gmail.com>
2023-05-17 23:39:19 +0200

    allowing top level async functions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+};
