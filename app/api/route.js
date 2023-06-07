7fb976a9776eea22225e4094e36c0c3627b75bab
yoricktf <yorick.tenfeld@gmail.com>
2023-06-07 12:23:09 +0200

    getting items from DB not able to map

--- app/api/route.js
+++ app/api/route.js
@@ -2,11 +2,14 @@ import dbConnect from '../../utils/dbConnect';
 import Resource from '../../models/resource';
 
 export async function GET(req) {
-  return new Response(JSON.stringify({ name: '--hello world' }));
+  const foundResources = await Resource.find();
+  // console.log('******************************', foundResources);
+  // return new Response(JSON.stringify({ name: '--hello world' }));
+  return new Response(JSON.stringify(foundResources));
 }
 export async function POST(req) {
   await dbConnect();
   const postObject = await req.json();
   const response = await Resource.create(postObject);
-  return new Response(JSON.stringify(postObject));
+  return new Response(JSON.stringify(response));
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import styles from './page.module.css';
 
 async function getData() {
   const res = await fetch('http://localhost:3000/api');
-  return res.json();
+  const data = await res.json();
+  return data;
 }
 
 async function sendData(data: object) {
@@ -13,18 +14,23 @@ async function sendData(data: object) {
   return res.json();
 }
 
-export default function Home() {
-  const test = getData();
-  const testSend = sendData({
-    title: 'test1',
-    description: 'testdescription',
-    url: 'testurl',
-    image: 'testimage',
-    category: ['testcategory'],
-  });
+export default async function Home() {
+  const resources = await getData();
+  // const testSend = sendData({
+  //   title: 'test1',
+  //   description: 'testdescription',
+  //   url: 'testurl',
+  //   image: 'testimage',
+  //   category: ['testcategory'],
+  // });
+
+  console.log('--------00000000000000-------------', resources.length);
   return (
     <main className={styles.main}>
       <h1>fuck</h1>
+      <h1>{resources[0].title}</h1>
+      <h1>{resources[7].title}</h1>
+      {/* map over the resource to diaplay their titles */}
     </main>
   );
 }
