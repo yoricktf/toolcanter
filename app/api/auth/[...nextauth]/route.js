af5666c55f9256af2d41db602f9eb5aa6b2475a0
yoricktf <yorick.tenfeld@gmail.com>
2023-08-15 16:05:46 +0200

    saving the user into the DB and passing information about the user to the session.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,24 +3,37 @@ import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import GithubProvider from 'next-auth/providers/github';
 import clientPromise from '../../../../utils/mongoDBAdapter.js';
 const authOptions = NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          email: profile.email,
+          name: profile.name,
+          userName: profile.login,
+          githubId: profile.id,
+          image: profile.avatar_url,
+          admin: false,
+        };
+      },
     }),
     // ...add more providers here
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-
-    async session({ session, token }) {
-      session.user = token;
+    async session({ session, user, token }) {
+      console.log('0000000000USER', user);
+      if (session?.user) {
+        session.user.id = user.id;
+        session.user.githubId = user.githubId;
+        session.user.email = user.email;
+        session.user.admin = user.admin;
+      }
       return session;
     },
   },
+  adapter: MongoDBAdapter(clientPromise),
 });
 
 // export default NextAuth(authOptions);
