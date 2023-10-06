204781b0878796b3b458a3c8ff02ecb8dd9a8c3c
yoricktf <yorick.tenfeld@gmail.com>
2023-10-06 13:39:47 +0200

    updated config to allow for images from loudinary and from example.com as sources are generated by chatGPT at the moment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'ghchart.rshah.org'],
+    domains: [
+      'avatars.githubusercontent.com',
+      'ghchart.rshah.org',
+      'res.cloudinary.com',
+      'example.com',
+    ],
   },
   reactStrictMode: true,
   // experimental: {
