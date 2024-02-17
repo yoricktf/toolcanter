673164dbf6e7c8178d61775f1594f1fd6b0d9f79
yoricktf <yorick.tenfeld@gmail.com>
2024-02-17 10:54:56 +0100

    displaying more data on the show page and getting other resources based on that pages category

--- app/resource/[id]/page.jsx
+++ app/resource/[id]/page.jsx
@@ -1,20 +1,70 @@
 import React from 'react';
 import dbConnect from '@/utils/dbConnect';
 import Resource from '@/models/Resource';
+import Image from 'next/image';
 
 const Page = async ({ params }) => {
   await dbConnect();
-  const resource = await Resource.findById(params?.id);
-  const { title, description, _id, url, image, category, createdAt } = resource;
+
+  const {
+    contributorsName,
+    contributorsPhoto,
+    title,
+    description,
+    _id,
+    url,
+    image,
+    categories,
+    createdAt,
+  } = await Resource.findById(params?.id);
+
+  console.log('categories', ...categories);
+
+  let similarResources = [];
+
+  for (const category of categories) {
+    console.log('------------category: ', category);
+    similarResources.push(
+      ...(await Resource.find({
+        categories: category,
+      }))
+    );
+    console.log('------------similarResources: ', similarResources);
+  }
 
   return (
     <>
-      <h1>{params.id}</h1>
-      <p>{title}</p>
-      <p>{description}</p>
-      <p>{url}</p>
-      <p>{image}</p>
-      <p>{category}</p>
+      <article>
+        <Image
+          src={image}
+          height={500}
+          width={500}
+          alt={`image of the ${title} resource`}
+        />
+        <Image
+          src={contributorsPhoto}
+          alt='Picture of the author'
+          className='avatar'
+          height={50}
+          width={50}
+        />
+        <h1>TITLE: {title}</h1>
+
+        <p>DESCRIPTION: {description}</p>
+        <p>URL: {url}</p>
+
+        <p>CATEGORIES: {categories}</p>
+      </article>
+      <section>
+        <h2>Similar Resources</h2>
+        {similarResources.map((resource) => (
+          <div key={resource._id}>
+            <h3>TITLE: {resource.title}</h3>
+            <p>DESCRIPTION: {resource.description}</p>
+            <p>CATEGORIES: {resource.categories}</p>
+          </div>
+        ))}
+      </section>
     </>
   );
 };
