31bf112cb7c7379fd5304afbbe2cdb2acc9901be
yoricktf <yorick.tenfeld@gmail.com>
2024-03-14 15:16:18 +0100

    using the new Tags and Resourcelist component and showing the 5 latest resources added

--- app/page.jsx
+++ app/page.jsx
@@ -2,37 +2,34 @@ import styles from './page.module.css';
 import dbConnect from '@/utils/dbConnect';
 import Resource from '@/models/Resource';
 import ResourceCard from '@/components/resourceCard';
-import Tag from '@/components/tag';
-
-const contentful = require('contentful');
+import Tags from '@/components/tags';
+import ResourcesList from '@/components/resourcesList';
 
 export default async function Home() {
   await dbConnect();
   const publishedResources = await Resource.find({ published: true });
-  const categories = await Resource.distinct('categories', { published: true });
+  const latestResources = await Resource.find({ published: true })
+    .sort({ createdAt: -1 }) // Sorting by createdAt field in descending order
+    .limit(5);
 
   return (
     <>
-      <header className={styles.header}>
+      <header className='header'>
         <h1>Yoz&apos;s Toolbox</h1>
-        <p className={styles.description}>
+        <p className='description'>
           this is a collection of <span>{publishedResources.length}</span> tools
           and resources that I have put together for myself and anyone else that
           is interested in Web Development
         </p>
       </header>
-      <div className={styles.resourcesBody}>
-        <div>
-          <menu className={styles.categories}>
-            {categories.map((categoryTitle) => (
-              <Tag key={categoryTitle} categoryTitle={categoryTitle} />
-            ))}
-          </menu>
-        </div>
-        <section className={`${styles.cards} cards`}>
-          {publishedResources.map((resource) => (
-            <ResourceCard key={resource._id} resource={resource} />
-          ))}
+
+      <div className='resourcesBody'>
+        <Tags />
+        <section className='newest'>
+          <h2>Latest Resources</h2>
+          <ResourcesList resources={latestResources} />
+          <h2>All Resources</h2>
+          <ResourcesList resources={publishedResources} />
         </section>
       </div>
     </>
