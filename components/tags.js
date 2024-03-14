014f0d7cfabab68217f0ceb06b406541e804dc13
yoricktf <yorick.tenfeld@gmail.com>
2024-03-14 15:12:20 +0100

    created a tags component

---
+++ components/tags.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Resource from '@/models/Resource';
+import styles from '../app/page.module.css';
+
+const Tags = async ({ currentCategory = 'all' }) => {
+  const categories = await Resource.distinct('categories', { published: true });
+
+  console.log('------------currentCategory: ', currentCategory);
+
+  return (
+    <menu className='categories'>
+      <Link href={`/`}>
+        <li className={currentCategory === 'all' ? 'active' : null}>All</li>
+      </Link>
+      {categories.map((categoryTitle) => (
+        <Link key={categoryTitle} href={`/category/${categoryTitle}`}>
+          <li className={categoryTitle === currentCategory ? 'active' : null}>
+            {categoryTitle}
+          </li>
+        </Link>
+      ))}
+    </menu>
+  );
+};
+
+export default Tags;
