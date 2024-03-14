104a5d410e7ce474ed704403b6938fc1ca972df1
yoricktf <yorick.tenfeld@gmail.com>
2024-03-14 15:17:13 +0100

    updated the page for showing the resources from different categories to look more similar to the home page and use the new componetns

--- app/category/[category]/page.jsx
+++ app/category/[category]/page.jsx
@@ -2,18 +2,29 @@ import dbConnect from '@/utils/dbConnect';
 import React from 'react';
 import Resource from '@/models/Resource';
 import ResourceCard from '@/components/resourceCard';
+import Tags from '@/components/tags';
+import styles from '../../page.module.css';
+import ResourcesList from '@/components/resourcesList';
 
 const Categories = async ({ params }) => {
   const { category } = params;
   await dbConnect();
   const filteredResources = await Resource.find({ categories: category });
-  console.log('filteredResources---------------:', filteredResources);
   return (
-    <div>
-      {filteredResources.map((resource) => {
-        return <ResourceCard key={resource._id} resource={resource} />;
-      })}
-    </div>
+    <>
+      <header className='header'>
+        <h1>{category} Resources</h1>
+        <p className='description'>
+          {filteredResources.length === 1
+            ? `There Is ${filteredResources.length} item here, why not add some more!`
+            : `There Are ${filteredResources.length} items here. `}
+        </p>
+      </header>
+      <div className='resourcesBody'>
+        <Tags currentCategory={category} />
+        <ResourcesList resources={filteredResources} />
+      </div>
+    </>
   );
 };
 
