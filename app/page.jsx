a604613ecbd5d988094cddca6a3d0d78e24661ba
yoricktf <yorick.tenfeld@gmail.com>
2024-03-20 16:16:11 +0100

    quick favorites on the homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,28 @@
 import styles from './page.module.css';
 import dbConnect from '@/utils/dbConnect';
 import Resource from '@/models/Resource';
-import ResourceCard from '@/components/resourceCard';
 import Tags from '@/components/tags';
 import ResourcesList from '@/components/resourcesList';
-
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/lib/auth';
+import User from '@/models/User';
+import Image from 'next/image';
+import Link from 'next/link';
 export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   await dbConnect();
   const publishedResources = await Resource.find({ published: true });
   const latestResources = await Resource.find({ published: true })
     .sort({ createdAt: -1 }) // Sorting by createdAt field in descending order
     .limit(5);
+
+  const { favorites } = await User.findById(session?.user.id, {
+    favorites: 1,
+    _id: 0,
+  }).populate('favorites');
+
+  console.log('------------userFavorites: ', favorites);
 
   return (
     <>
@@ -21,6 +33,20 @@ export default async function Home() {
           and resources that I have put together for myself and anyone else that
           is interested in Web Development
         </p>
+        <section className='quickFavorites'>
+          {favorites &&
+            favorites.map((resource) => (
+              <Link href={`/resource/${resource.id}`} key={resource.title}>
+                <Image
+                  src={resource.image}
+                  key={resource.title}
+                  width={25}
+                  height={25}
+                  alt='test'
+                />
+              </Link>
+            ))}
+        </section>
       </header>
 
       <div className='resourcesBody'>
